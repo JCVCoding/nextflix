b86c2f3063445a50c53fb18b5474a81e3e033bee
josh <jvcoding9407@gmail.com>
2024-06-24 14:28:53 -0700

    update roboto slab font weight globally, create banner component and its styling

---
+++ components/banner/banner.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from "./banner.module.css";
+
+type BannerProps = {
+  title: string;
+  subTitle: string;
+  imgUrl: string;
+};
+
+const Banner = ({ title, subTitle, imgUrl }: BannerProps) => {
+  const handleOnPlay = () => {};
+  return (
+    <div className={styles.container}>
+      <div className={styles.leftWrapper}>
+        <div className={styles.left}>
+          <div className={styles.nseriesWrapper}>
+            <p className={styles.firstLetter}>N</p>
+            <p className={styles.series}>S E R I E S</p>
+          </div>
+          <h3 className={styles.title}>{title}</h3>
+          <h3 className={styles.subTitle}>{subTitle}</h3>
+          <div className={styles.playBtnWrapper}>
+            <button className={styles.btnWithIcon} onClick={handleOnPlay}>
+              <span className={styles.playText}>Play</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div
+        className={styles.bannerImg}
+        style={{
+          backgroundImage: `url(${imgUrl})`,
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          backgroundSize: "cover",
+          backgroundPosition: "50% 50%",
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default Banner;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
+import Banner from "@/components/banner/banner";
 
 export default function Home() {
   return (
@@ -9,6 +10,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Nextflix</h1>
+      {/* <NavBar /> */}
+      <Banner
+        title="Clifford the red dog"
+        subTitle="a very cute dog"
+        imgUrl="/static/clifford.webp"
+      />
+      {/* <Card /> */}
     </div>
   );
 }
