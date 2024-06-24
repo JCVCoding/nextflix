ecd64d25047fdb26c2dc0807d33184118dc58291
josh <jvcoding9407@gmail.com>
2024-06-24 16:49:55 -0700

    Create card component/styling and add to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import Banner from "@/components/banner/banner";
 import NavBar from "@/components/nav/navbar";
+import Card from "@/components/card/card";
 
 export default function Home() {
   return (
@@ -16,7 +17,7 @@ export default function Home() {
         subTitle="a very cute dog"
         imgUrl="/static/clifford.webp"
       />
-      {/* <Card /> */}
+      <Card />
     </div>
   );
 }
