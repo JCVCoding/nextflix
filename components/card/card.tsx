de9e3e9098bb1cdbb194f2788233a9135f3ac42b
josh <jvcoding9407@gmail.com>
2024-06-25 09:44:46 -0700

    Add error handling to card to take care of incorrect imgUrls

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -1,25 +1,40 @@
 import Image from "next/image";
 import styles from "./card.module.css";
+import { useState } from "react";
 
 type CardType = {
   imgUrl: string;
-  size: string;
+  size: "small" | "medium" | "large";
 };
 
-const Card = ({ imgUrl, size }: CardType) => {
+const Card = ({
+  imgUrl = "https://images.unsplash.com/photo-1485846234645-a62644f84728?q=80&w=2959&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  size = "medium",
+}: CardType) => {
   const classMap: { [key: string]: string } = {
     large: styles.lgItem,
     medium: styles.mdItem,
     small: styles.smItem,
   };
+
+  const [imgSrc, setImgSrc] = useState(imgUrl);
+
+  const handleError = (event: any) => {
+    console.log(event);
+    setImgSrc(
+      "https://images.unsplash.com/photo-1485846234645-a62644f84728?q=80&w=2959&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    );
+  };
+
   return (
     <div className={styles.container}>
       <div className={classMap[size]}>
         <Image
-          src={imgUrl}
+          src={imgSrc}
           alt="image"
           layout="fill"
           className={styles.cardImg}
+          onError={handleError}
         />
       </div>
     </div>
