de61fcfdc71676157e8c1519e0dd78f7e72ae42b
josh <jvcoding9407@gmail.com>
2024-06-24 17:14:13 -0700

    Add props to Card component (imgUrl, size) on home page

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import Image from "next/image";
+import styles from "./card.module.css";
 
-const Card = () => {
-  return <div>Card</div>;
+type CardType = {
+  imgUrl: string;
+  size: string;
+};
+
+const Card = ({ imgUrl, size }: CardType) => {
+  const classMap: { [key: string]: string } = {
+    large: styles.lgItem,
+    medium: styles.mdItem,
+    small: styles.smItem,
+  };
+  return (
+    <div className={styles.container}>
+      <div className={classMap[size]}>
+        <Image
+          src={imgUrl}
+          alt="image"
+          layout="fill"
+          className={styles.cardImg}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Card;
