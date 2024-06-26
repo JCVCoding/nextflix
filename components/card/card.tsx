58b59cdbe72f56b8c9d29aedf43158ca02a88c3f
josh <jvcoding9407@gmail.com>
2024-06-26 15:17:38 -0700

    Refactor error handler, refactor card types

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -4,10 +4,12 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import cls from "classnames";
 
-type CardType = {
+export type CardSizes = "small" | "medium" | "large";
+
+export type CardType = {
   imgUrl: string;
-  size: "small" | "medium" | "large";
-  id: number;
+  size?: CardSizes;
+  id?: number;
 };
 
 const Card = ({
@@ -23,7 +25,7 @@ const Card = ({
 
   const [imgSrc, setImgSrc] = useState(imgUrl);
 
-  const handleError = (event: any) => {
+  const handleError = () => {
     setImgSrc(
       "https://images.unsplash.com/photo-1485846234645-a62644f84728?q=80&w=2959&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     );
