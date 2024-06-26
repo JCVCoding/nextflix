3498110105a62b6c5a76cc7552130d208504ca3b
josh <jvcoding9407@gmail.com>
2024-06-26 15:19:18 -0700

    Refactor section cards component and update the home page by removing cards and using the section cards component to populate the cards on the page

--- components/card/section-cards.tsx
+++ components/card/section-cards.tsx
@@ -1,17 +1,21 @@
 import React from "react";
-import Card from "./card";
+import Card, { CardSizes, CardType } from "./card";
 import styles from "./section-cards.module.css";
 
 type SectionCardsType = {
   title: string;
+  videos: Array<CardType>;
+  size: CardSizes;
 };
 
-const SectionCards = ({ title }: SectionCardsType) => {
+const SectionCards = ({ title, videos, size }: SectionCardsType) => {
   return (
     <section className={styles.container}>
       <h2 className={styles.title}>{title}</h2>
       <div className={styles.cardWrapper}>
-        <Card imgUrl="/static/clifford.webp" size="large" />
+        {videos.map((video, index) => (
+          <Card imgUrl={video.imgUrl} size={size} id={index} key={video.id} />
+        ))}
       </div>
     </section>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,16 @@ import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import Banner from "@/components/banner/banner";
 import NavBar from "@/components/nav/navbar";
-import Card from "@/components/card/card";
+import Card, { CardType } from "@/components/card/card";
 import SectionCards from "@/components/card/section-cards";
 
 export default function Home() {
+  const disneyVideos: Array<CardType> = [
+    { imgUrl: "/static/clifford.webp" },
+    { imgUrl: "/static/clifford.webp" },
+    { imgUrl: "/static/clifford.webp" },
+    { imgUrl: "/static/clifford.webp" },
+  ];
   return (
     <div className={styles.container}>
       <Head>
@@ -18,10 +24,11 @@ export default function Home() {
         subTitle="a very cute dog"
         imgUrl="/static/clifford.webp"
       />
-      <SectionCards title="Disney" />
-      <Card imgUrl="/static/clifford.webp" size="large" />
-      <Card imgUrl="/static/clifford.webp" size="medium" />
-      <Card imgUrl="/static/clifford.webp" size="small" />
+      <div className={styles.sectionWrapper}>
+        <SectionCards title="Disney" videos={disneyVideos} size="large" />
+        <SectionCards title="Disney" videos={disneyVideos} size="medium" />
+        <SectionCards title="Disney" videos={disneyVideos} size="small" />
+      </div>
     </div>
   );
 }
