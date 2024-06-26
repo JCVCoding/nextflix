75685a5ec09d1c3591f7ebd2ae65187f411f350b
josh <jvcoding9407@gmail.com>
2024-06-26 12:31:41 -0700

    Add id to card component, update layout prop to fill on Image, create scale condtional var

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -7,11 +7,13 @@ import cls from "classnames";
 type CardType = {
   imgUrl: string;
   size: "small" | "medium" | "large";
+  id: number;
 };
 
 const Card = ({
   imgUrl = "https://images.unsplash.com/photo-1485846234645-a62644f84728?q=80&w=2959&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   size = "medium",
+  id,
 }: CardType) => {
   const classMap: { [key: string]: string } = {
     large: styles.lgItem,
@@ -22,22 +24,23 @@ const Card = ({
   const [imgSrc, setImgSrc] = useState(imgUrl);
 
   const handleError = (event: any) => {
-    console.log(event);
     setImgSrc(
       "https://images.unsplash.com/photo-1485846234645-a62644f84728?q=80&w=2959&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     );
   };
 
+  const scale = id === 0 ? { scaleY: 1.1 } : { scale: 1.1 };
+
   return (
     <div className={styles.container}>
       <motion.div
         className={cls(styles.imgMotionWrapper, classMap[size])}
-        whileHover={{ scale: 1.2 }}
+        whileHover={{ ...scale }}
       >
         <Image
           src={imgSrc}
           alt="image"
-          layout="fill"
+          fill
           className={styles.cardImg}
           onError={handleError}
         />
