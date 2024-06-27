cf197f4a3ad780342bf918f9e92c656c64c92018
josh <jvcoding9407@gmail.com>
2024-06-27 11:53:02 -0700

    If user is logged in then the app will route user to home page, otherwise user will be routed to Log In page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,38 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
+import { magic } from "@/lib/magic-client";
+import { useRouter } from "next/router";
+import Loading from "@/components/loading/loading";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const routeUser = async () => {
+      const isUserLoggedIn = await magic?.user.isLoggedIn();
+      if (isUserLoggedIn) {
+        router.push("/");
+      } else {
+        router.push("/login");
+      }
+    };
+    routeUser();
+  }, [router]);
+
+  useEffect(() => {
+    const handleComplete = () => {
+      setIsLoading(false);
+    };
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, [router]);
+
+  return isLoading ? <Loading /> : <Component {...pageProps} />;
 }
