34a75c65cff951f97784b611b29c556dea3ee038
josh <jvcoding9407@gmail.com>
2024-07-06 17:54:19 -0700

    Add my list page styling

--- pages/browse/my-list.tsx
+++ pages/browse/my-list.tsx
@@ -2,15 +2,17 @@ import SectionCards from "@/components/card/section-cards";
 import NavBar from "@/components/nav/navbar";
 import Head from "next/head";
 
+import styles from "../../styles/my-list.module.css";
+
 const MyList = () => {
   return (
     <div>
       <Head>
         <title>My List</title>
       </Head>
-      <main>
+      <main className={styles.main}>
         <NavBar />
-        <div>
+        <div className={styles.sectionWrapper}>
           <SectionCards title={"My List"} videos={[]} size={"small"} />
         </div>
       </main>
